3fc235844af994f226202db124c7332b7d725d86
PresDev <andrewindiedev@gmail.com>
2024-04-24 06:22:52 +1000

    Update Product.cs

--- Assets/_Game/Scripts/Product.cs
+++ Assets/_Game/Scripts/Product.cs
@@ -59,7 +59,7 @@ public class Product : BaseInteractable
         if (!IsHeld) return;
         isHeld = false;
         UnFreezeProduct();
-        ReleaseOwnershipServerRPC(Camera.main.transform.forward * 2);
+        ReleaseOwnershipServerRPC(Camera.main.transform.forward * 7);
     }
     #endregion
 
